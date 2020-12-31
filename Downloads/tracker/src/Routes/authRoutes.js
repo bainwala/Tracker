0a933d50c5c30e342577ee59f5033791c0cbe815
Himanshu Bainwala <hbainwala@gmail.com>
2020-12-31 15:09:20 -0800

    second

--- Downloads/tracker/src/Routes/authRoutes.js
+++ Downloads/tracker/src/Routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 const User = mongoose.model('User')
 const router = express.Router();
 
@@ -8,9 +9,32 @@ router.post('/signup', async (req, res) => {
     try {
         const user = new User({email,password})
         await user.save()
-        res.send('Signed Up!')
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY' )
+        res.send({token})
     } catch (error) {
         res.status(422).send(error.message)
+    }  
+})
+
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email || !password) {
+        return res.status(422).send({error: 'Must provide email and password'})
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+        return res.status(422).send({error: 'Invalid Password or Email'})
+    }
+
+    try {
+        await user.comparePassword(password)
+        const token = jwt.sign({userId:user._id}, 'MY_SECRET_KEY')
+        res.send({token})
+    } catch (error) {
+        return res.status(422).send({error: 'Invalid Password or Email'})
     }
     
 })
--- Downloads/tracker/src/index.js
+++ Downloads/tracker/src/index.js
@@ -1,8 +1,11 @@
 require('./models/User')
+require('./models/Track')
 const express = require('express');
 const app = express();
 const authRoutes = require('./Routes/authRoutes')
+const trackRoutes = require('./Routes/trackRoutes')
 const bodyParser = require('body-parser')
+const requireAuth = require('./middlewares/requireAuth')
 
 
 const mongoose = require('mongoose')
@@ -10,6 +13,7 @@ const MongoURI = 'mongodb+srv://admin:admin_password@cluster0.vf4s5.mongodb.net/
 
 app.use(bodyParser.json())
 app.use(authRoutes)
+app.use(trackRoutes)
 
 mongoose.connect(MongoURI, {
     useNewUrlParser: true,
@@ -25,8 +29,8 @@ mongoose.connection.on('error', (err) => {
     console.log('Error connecting to Mongo', err)
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/', requireAuth ,(req, res) => {
+    res.send(`Your email: ${req.user.email}`)
 })
 
 app.listen('3000', () => {
